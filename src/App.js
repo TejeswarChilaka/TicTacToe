ce3b33f367a357a6de9b42f0faf530b74fb2bfa7
TejeswarChilaka <chilaka.tejeswar@gmail.com>
2024-02-22 12:59:19 +0530

    Starting Tic Tac Toe

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function Square(){
+  return(
+    
+    <>
+    <button className='square'>X</button>
+    </>
+  )
+}
+
+
+
+
+
+function App() {
+  return (
+    <div className='App'>
+    <Square></Square>
+    </div>
+  );
+}
+
+export default App;
